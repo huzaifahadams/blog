c5b7551849c3da4ddae312259bf9f5a0cee5cf99
huzaifahadams <huzaifah.adams@gmail.com>
2022-01-21 13:55:17 +0300

    commit

---
+++ myblog/adams_blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdamsBlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'adams_blog'
---
+++ myblog/adams_blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls.conf import path
+from .import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('post/<str:pk>', views.post, name='post')
+
+]
